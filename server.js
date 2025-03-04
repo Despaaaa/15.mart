d153cf43239ca2ea67aceb30651cd029b7a79a3b
Despaaaa <alexdespot0007@gmail.com>
2025-03-04 02:27:16 +0100

    Fix static file serving

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the public directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Parse JSON bodies
 app.use(express.json());
